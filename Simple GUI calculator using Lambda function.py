2fc9e06b2a6c634a824f50022bfe69d20022be3f
ROOTHIE1000 <44506915+ROOTHIE1000@users.noreply.github.com>
2020-08-26 22:25:57 +0100

    Update Simple GUI calculator using Lambda function.py

--- Simple GUI calculator using Lambda function.py
+++ Simple GUI calculator using Lambda function.py
@@ -1,8 +1,8 @@
-from tkinter import*
+from tkinter import* # Importing Tkinter
 
-def BtnClick(numbers):
-    global operator
-    operator = operator +str(numbers)
+def ButtonClick(numbers):  # funtion for the Button clicks
+    global operator   # naming 'operator' a global variable
+    operator = operator +str(numbers) # converting number 
     text_input.set(operator)
     
 
@@ -29,53 +29,53 @@ textDisplay = Entry(calc,font=("gandugi",20, "bold"), textvariable=text_input, b
                                  bg="thistle", justify="right").grid(columnspan=4)
 
 
-Btn7=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "7",command=lambda:BtnClick(7)).grid(row=1,column=0)
+Button_7=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
+            text = "7",command=lambda:ButtonClick(7)).grid(row=1,column=0)
             
-Btn8=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "8",command=lambda:BtnClick(8)).grid(row=1,column=1)
+Button_8=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
+            text = "8",command=lambda:ButttonClick(8)).grid(row=1,column=1)
 
-Btn9=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "9", command=lambda:BtnClick(9)).grid(row=1,column=2)
+Button_9=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
+            text = "9", command=lambda:ButttonClick(9)).grid(row=1,column=2)
 
-Addition=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "+",command=lambda:BtnClick("+")).grid(row=1,column=3)
+Button_Addition=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
+            text = "+",command=lambda:ButttonClick("+")).grid(row=1,column=3)
 
 #==================================================================
 Btn4=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "4",command=lambda:BtnClick(4)).grid(row=2,column=0)
+            text = "4",command=lambda:ButttonClick(4)).grid(row=2,column=0)
 
 Btn5=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "5",command=lambda:BtnClick(5)).grid(row=2,column=1)
+            text = "5",command=lambda:ButttonClick(5)).grid(row=2,column=1)
 
 Btn=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "6",command=lambda:BtnClick(6)).grid(row=2,column=2)
+            text = "6",command=lambda:ButttonClick(6)).grid(row=2,column=2)
 
 Subtraction=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "-",command=lambda:BtnClick("-")).grid(row=2,column=3)
+            text = "-",command=lambda:ButttonClick("-")).grid(row=2,column=3)
 
 #====================================================================
 Btn1=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "1",command=lambda:BtnClick(1)).grid(row=3,column=0)
+            text = "1",command=lambda:ButttonClick(1)).grid(row=3,column=0)
 
 Btn2=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "2",command=lambda:BtnClick(2)).grid(row=3,column=1)
+            text = "2",command=lambda:ButttonClick(2)).grid(row=3,column=1)
 
 Btn3=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "3",command=lambda:BtnClick(3)).grid(row=3,column=2)
+            text = "3",command=lambda:ButttonClick(3)).grid(row=3,column=2)
 
 Multiplication=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "*",command=lambda:BtnClick("*")).grid(row=3,column=3)
+            text = "*",command=lambda:ButttonClick("*")).grid(row=3,column=3)
 
 #======================================================================
 Btn0=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "0",command=lambda:BtnClick(0)).grid(row=4,column=0)
+            text = "0",command=lambda:ButttonClick(0)).grid(row=4,column=0)
 
 BtnClear=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "C",command=BtnClearDisplay).grid(row=4,column=1)
+            text = "C",command=ClearDisplayButton).grid(row=4,column=1)
 
 BtnEquals=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
-            text = "=",command=BtnEqualsInput).grid(row=4,column=2)
+            text = "=",command=EqualsInputButton).grid(row=4,column=2)
 
 BtnDivision=Button(calc,padx=16,bd=8, fg="black",font=("arial",20, "bold"),
             text = "/",command=lambda:BtnClick("/")).grid(row=4,column=3)
